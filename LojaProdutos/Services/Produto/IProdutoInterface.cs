417f5a7777a5b3cdd1c24651957ca7c61b75e0d8
Junior <ti.junior@gmail.com>
2025-04-03 22:01:49 -0300

    Criando tabelas produtos e categorias

---
+++ LojaProdutos/Services/Produto/IProdutoInterface.cs
@@ -0,0 +1,9 @@
+﻿using LojaProdutos.Models;
+
+namespace LojaProdutos.Services.Produto
+{
+    public interface IProdutoInterface
+    {
+        Task<List<ProdutoModel>> BuscarProdutos();
+    }
+}
