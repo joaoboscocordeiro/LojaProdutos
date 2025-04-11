c3c7458f6a7b36a696b2bf3db972175a01980462
João Bosco <ti.junior@gmail.com>
2025-04-10 22:39:33 -0300

    Configurando cadastro

--- LojaProdutos/obj/Debug/net8.0/LojaProdutos.AssemblyInfo.cs
+++ LojaProdutos/obj/Debug/net8.0/LojaProdutos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaProdutos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c160b8f969d321c1a272d236292fa89a4887c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e155da359bf9c6450d8136324fd1a9cbaa527cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaProdutos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaProdutos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
